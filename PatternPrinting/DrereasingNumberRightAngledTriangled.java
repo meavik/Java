ba9bdc93b6941514cfd5c0972a8d155df2d53ba1
ShaunAshraf <33549886+meavik@users.noreply.github.com>
2021-02-18 00:06:37 +0600

    Create DrereasingNumberRightAngledTriangled.java

---
+++ PatternPrinting/DrereasingNumberRightAngledTriangled.java
@@ -0,0 +1,17 @@
+package basicjava;
+        
+public class BasicJava{ 
+    public static void main(String []args){
+        int n = 5;
+        int i,j;
+        for(i =1;i<=n;i++){
+            for(j =1; j<=i;j++){
+                System.out.print("  ");
+            }
+            for(j=i;j<=n;j++){
+            System.out.print("* ");
+            }
+            System.out.println();
+        }
+}
+} 
