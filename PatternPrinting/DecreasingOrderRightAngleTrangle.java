97e2d9cc898286fff8ab2b9e691e3ada702ae6eb
ShaunAshraf <33549886+meavik@users.noreply.github.com>
2021-02-17 23:59:32 +0600

    Create DecreasingOrderRightAngleTrangle.java

---
+++ PatternPrinting/DecreasingOrderRightAngleTrangle.java
@@ -0,0 +1,14 @@
+package basicjava;
+        
+public class BasicJava{ 
+    public static void main(String []args){
+        int n = 5;
+        int i,j;
+        for(i =1;i<=n;i++){
+            for(j =i; j<=n;j++){
+                System.out.print("*  ");
+            }
+            System.out.println();
+        }
+}
+} 
